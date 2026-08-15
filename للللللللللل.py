13b09ed1358717beb881befd0c5923b6d426a74d
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-15 14:17:25 -0700

    Update للللللللللل.py

--- للللللللللل.py
+++ للللللللللل.py
@@ -131,7 +131,9 @@ else:
  
  st.toast("هيا في تحدي 60 ثانيه ")
  st.write(num1,sign,num2)
-button=st.form_submit_button("تأكيد الاجابه")
+with st.form:
+  button=st.form_submit_button("تأكيد الاجابه")
+
 
 time1=st.progress(100)
 for sec in range (60,-1,-1):
