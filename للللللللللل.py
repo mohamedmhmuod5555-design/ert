e6ba4338624510e7596212ff001cce5e66d9913d
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-15 14:49:55 -0700

    Update للللللللللل.py

--- للللللللللل.py
+++ للللللللللل.py
@@ -136,9 +136,9 @@ else:
    number=st.number_input("ادخل النتيجه ",step=1,key="60 sec1 challenge")
  if button :
     st.session_state.count += 1
-  if number == sc:
-    st.session_state.num += 1
-    st.session_state.feed="correct"
+ if number == sc:
+  st.session_state.num += 1
+  st.session_state.feed="correct"
   else:
     st.session_state.feed="false" 
 if st.session_state.feed=="correct":
