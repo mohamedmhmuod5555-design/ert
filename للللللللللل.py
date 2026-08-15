d49db6e39105e41e849ec26aadb58d7b703d3f50
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-15 14:54:21 -0700

    Update للللللللللل.py

--- للللللللللل.py
+++ للللللللللل.py
@@ -159,10 +159,10 @@ if st.session_state.feed=="false":
   time.sleep(1)
   st.rerun()
   time1=st.progress(100)
- for sec in range (60,-1,-1):
-   time.sleep(1)
-   percentage=(float(sec/60))
-   time1.progress(percentage,text=f" الثواني المتبقيه {sec}")
+  for sec in range (60,-1,-1):
+    time.sleep(1)
+    percentage=(float(sec/60))
+    time1.progress(percentage,text=f" الثواني المتبقيه {sec}")
     
 
  
